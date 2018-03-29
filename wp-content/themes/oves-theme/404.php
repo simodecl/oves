b750049eedb0253af76f5e9f158205c7cc3f1cd5
simodecl <siodecl@student.arteveldehs.be>
2018-03-29 11:14:36 +0200

    db update oves

--- wp-content/themes/oves-theme/404.php
+++ wp-content/themes/oves-theme/404.php
@@ -2,14 +2,7 @@
 get_header();
 ?>
 
-<div id="main">
-    <div class="fourOhFour_Container">
-        <h1 class="fourOhFour_title">404...</h1><h2 class="fourOhFour_subtitle">Hiccup.. Oh no!</h2>
-        <p class="fourOhFour_body">
-        It seems our server got a bit drunk and messed up a bit while processing your request.
-    or maybe there wasn't anything here to begin with. Maybe we should give it more alcohol free beer?
-    For now, you're probably better going back to the homepage.
-        </p>
-    </div>
+<div style="margin-bottom:20%" class="pageTitle">
+    Helaas....Deze pagina is niet beschikbaar!
 </div>
 <?php get_footer(); ?>
